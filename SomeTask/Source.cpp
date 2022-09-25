2d9a448ccc6ff9ee2b9f2bf5fbe87c7d04830b40
Elvin Camalzade <elvincamalzade00@gmail.com>
2022-09-25 09:43:11 +0400

    I add showStudent functions

--- SomeTask/Source.cpp
+++ SomeTask/Source.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
-
+#include "Entities.h"
 using namespace std;
+
+
+void ShowStudent(const Student& student) {
+	cout << "Name : " << student.name << endl;
+	cout << "Surname : " << student.surname << endl;
+	cout << "Age : " << student.age << endl;
+}
 
 void main() {
 
